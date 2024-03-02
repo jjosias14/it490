cf337bd7bc222e5fc401b2f4f7c01fab5a8ca3ae
RJ Tyrrell <tyrrellr50@gmail.com>
2024-03-02 14:11:52 -0500

    start of database functions in php

---
+++ back/databaseFunctions.php
@@ -0,0 +1,14 @@
+#!/usr/bin/php
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+require_once('signIn.php.inc');
+require_once('dataBaseconnect.php');
+require_once('databaseClient.php');
+
+function getUser()
+{
+
+}
+?>
